f59348f241fc966e9103a3a5c6079d56827f3a3b
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-07-15 14:50:45 -0400

    finished up nav bar and some of the front end design for home page for now

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -21,9 +21,10 @@ function Navigation() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -33,13 +34,17 @@ function Navigation() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
+            <Nav.Link href="#resume">Resume</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <Nav.Link href="#services">
+              <span id="services-nav-link">Services</span>
             </Nav.Link>
+
+            {/* <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
