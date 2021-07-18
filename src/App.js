c98f80cebc4815c53cbd8cd507ee1c0a63ad031a
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-07-18 13:30:31 -0400

    Found a video on aos a animations on scrolling dependency. Seems promising to use for fade and slide in animations. Also replaced my portfolio page back to normal

--- src/App.js
+++ src/App.js
@@ -3,17 +3,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar";
-// import Portfolio from "./pages/portfolio";
-import { useRef } from "react";
-import { useIntersection } from "react-use";
-// Animation library
-import gsap from "gsap";
+import Portfolio from "./pages/portfolio";
 
-import VideoCarousel from "./components/VideoCarousel";
-import AboutHeader from "./components/AboutHeader";
-import About from "./components/About";
-import ProjectsHeader from "./components/ProjectsHeader";
-import Projects from "./components/Projects";
 function Wrapper(props) {
   return (
     <>
@@ -30,12 +21,7 @@ function App() {
         <div className="background-color">
           <Switch>
             <Wrapper>
-              <Route exact path="/" />
-              <VideoCarousel></VideoCarousel>
-              <AboutHeader></AboutHeader>
-              <About></About>
-              <ProjectsHeader></ProjectsHeader>
-              <Projects></Projects>
+              <Route exact path="/" component={Portfolio} />
             </Wrapper>
           </Switch>
         </div>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,19 +1,19 @@
-// import React from "react";
-// import VideoCarousel from "../components/VideoCarousel";
-// import AboutHeader from "../components/AboutHeader";
-// import About from "../components/About";
-// import ProjectsHeader from "../components/ProjectsHeader";
-// import Projects from "../components/Projects";
+import React from "react";
+import VideoCarousel from "../components/VideoCarousel";
+import AboutHeader from "../components/AboutHeader";
+import About from "../components/About";
+import ProjectsHeader from "../components/ProjectsHeader";
+import Projects from "../components/Projects";
 
-// function Portfolio() {
-//   return (
-//     <div>
-//       <VideoCarousel></VideoCarousel>
-//       <AboutHeader></AboutHeader>
-//       <About></About>
-//       <ProjectsHeader></ProjectsHeader>
-//       <Projects></Projects>
-//     </div>
-//   );
-// }
-// export default Portfolio;
+function Portfolio() {
+  return (
+    <div>
+      <VideoCarousel></VideoCarousel>
+      <AboutHeader></AboutHeader>
+      <About></About>
+      <ProjectsHeader></ProjectsHeader>
+      <Projects></Projects>
+    </div>
+  );
+}
+export default Portfolio;
