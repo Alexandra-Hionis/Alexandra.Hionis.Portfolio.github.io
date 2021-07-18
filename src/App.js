ba2a9e6d8df95730e4e7b0dcb4bbf40fcb5fa445
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-07-18 11:21:37 -0400

    installed 2 new libraries, react use and react gsap which is an animation library to use for fading or sliding in animation. I want my paragraphs or photos to be animated when scrolled. Also eliminated the portoflio page for now and placed everything in my app.js file instead

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar";
-import Portfolio from "./pages/portfolio";
+// import Portfolio from "./pages/portfolio";
+import { useRef } from "react";
+import { useIntersection } from "react-use";
+// Animation library
+import gsap from "gsap";
 
+import VideoCarousel from "./components/VideoCarousel";
+import AboutHeader from "./components/AboutHeader";
+import About from "./components/About";
+import ProjectsHeader from "./components/ProjectsHeader";
+import Projects from "./components/Projects";
 function Wrapper(props) {
   return (
     <>
@@ -21,7 +30,12 @@ function App() {
         <div className="background-color">
           <Switch>
             <Wrapper>
-              <Route exact path="/" component={Portfolio} />
+              <Route exact path="/" />
+              <VideoCarousel></VideoCarousel>
+              <AboutHeader></AboutHeader>
+              <About></About>
+              <ProjectsHeader></ProjectsHeader>
+              <Projects></Projects>
             </Wrapper>
           </Switch>
         </div>
