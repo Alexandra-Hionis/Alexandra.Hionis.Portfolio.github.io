511a53a42b2e22dd35f10d8ce520f9d015b5e108
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2022-02-02 21:52:06 -0500

    added COMING SOON: to wordpress projects

--- src/components/Projects/projectDataWP.js
+++ src/components/Projects/projectDataWP.js
@@ -1,6 +1,6 @@
 export const projectswp = [
   {
-    title: "Fireside Service & Installation",
+    title: "COMING SOON: Fireside Service & Installation",
     subtitle: "WordPress, Elementor, HTML, CSS",
     description:
       "Business website for Fireside Service & Installation located in Traverse City, Minnesota. With over 25 years of experience in the fireplace installation industry, they strive to be the complete hearth store, specializing in all aspects of the hearth industry as well as outdoor kitchens and outdoor heating. Built with the help of my work team at Hooker & Company Advertising.",
@@ -8,7 +8,7 @@ export const projectswp = [
     link1: "#",
   },
   {
-    title: "Hearthcrest Fireplace",
+    title: "COMING SOON: Hearthcrest Fireplace",
     subtitle: "WordPress, Elementor, HTML, CSS",
     description:
       "Website for Hearthcrest Fireplace, a business that provides quality hearth products in Minnesota. Built with the help of my work team at Hooker & Company Advertising.",
