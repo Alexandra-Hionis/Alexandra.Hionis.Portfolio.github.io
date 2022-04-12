c85b3e6ff4f12a0e6254c548a9ca6fd7d5b8cefa
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2022-04-12 14:16:16 -0400

    cdt added to projects

--- src/components/Projects/projectDataWP.js
+++ src/components/Projects/projectDataWP.js
@@ -1,7 +1,7 @@
 export const projectswp = [
   {
     title: "COMING SOON: Fireside Service & Installation",
-    subtitle: "WordPress, Elementor, HTML, CSS",
+    subtitle: "WordPress, Elementor, HTML, CSS, Javascript",
     description:
       "Business website for Fireside Service & Installation located in Traverse City, Minnesota. With over 25 years of experience in the fireplace installation industry, they strive to be the complete hearth store, specializing in all aspects of the hearth industry as well as outdoor kitchens and outdoor heating. Built with the help of my work team at Hooker & Company Advertising.",
     image: "./images/firesideservices.png",
@@ -9,7 +9,7 @@ export const projectswp = [
   },
   {
     title: "COMING SOON: Hearthcrest Fireplace",
-    subtitle: "WordPress, Elementor, HTML, CSS",
+    subtitle: "WordPress, Elementor, HTML, CSS, Javascript",
     description:
       "Website for Hearthcrest Fireplace, a business that provides quality hearth products in Minnesota. Built with the help of my work team at Hooker & Company Advertising.",
     image: "./images/hearthcrest.png",
@@ -17,10 +17,18 @@ export const projectswp = [
   },
   {
     title: "COMING SOON: Asset HR",
-    subtitle: "WordPress, Elementor, HTML, CSS",
+    subtitle: "WordPress, Elementor, HTML, CSS, Javascript",
     description:
       "Website for Asset HR, a human resources consulting firm. Built with the help of my work team at Hooker & Company Advertising.",
     image: "./images/assethr.png",
+    link1: "#",
+  },
+  {
+    title: "COMING SOON: Chanhassen Dinner Theatres Weddings & Events",
+    subtitle: "WordPress, Elementor, HTML, CSS, Javascript",
+    description:
+      "Weddings and events website for Chanhassen Dinner Theatres. A performance & dining venue pairing musical theater & cabaret shows with classic American meals.",
+    image: "./images/CDT.png",
     link1: "#",
   },
 ];
